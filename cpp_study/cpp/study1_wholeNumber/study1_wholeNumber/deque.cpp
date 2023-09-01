1c0d5f10a7a63f059dabbcbe59ec7a829abe3d9a
penguin821 <penguin821@naver.com>
2023-09-01 16:36:27 +0900

    deque ing

---
+++ cpp_study/cpp/study1_wholeNumber/study1_wholeNumber/deque.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// 시퀀스 컨테이너 : 데이터가 삽입 순서대로 나열되는 형태
+	// 종류 : vector list deque
+
+	// deque : double ended queue 데크
+	// 가끔 서버 버퍼 만들어줄때 사용하는 사람이 있긴함
+	// 벡터처럼 한 덩어리 형태로 사용되나 
+	return 0;
+}
